4104954fa10103be2796564a8325137e43f38873
chelsigorridon <chelsijaimegorridon@gmail.com>
2026-02-04 15:24:07 +0200

    Added Loops , Else statement

--- src/script.js
+++ src/script.js
@@ -25,23 +25,33 @@ const prompt = require("prompt-sync")(); // this is used to allow the user to in
 
 /* This is a shorthand way of writing the same function as above using an arrow function */
 const Deposit =() => {
+
+    while (true) {  /* While creates an infinite loop meaning the user will be prompted to enter a deposit amount until they enter a valid amount 
+        and true is the condition in this case we want it to be true cause if the 
+        user keeps putting in an invalid input the same message will keep appearing */ 
+
     const Despositamount = prompt  ("Enter a deposit amount : ");
     const NumberDepositamount = parseFloat (Despositamount); 
     /* Parse converts the string input from the user into a number. parseFloat 
     ensures it can be used in calculations later on and it ensure decimals are added */
 
-    if (isNaN (NumberDepositamount) || NumberDepositamount < 0) {
-        console.log("Invalid deposit amount, try again. ");
-    }
-
     /* If statements are used to do checks on the user input to ensure it is valid
     isNaN checks if the input is not a number || this means OR if what the user input it is < less than 0
     if we console log it the user will get a message saying invalid */
   
+    if (isNaN (NumberDepositamount) || NumberDepositamount < 0) {
+    
+        console.log ("Invalid deposit amount, try again. ");
+
+     } else { /* if the user input is valid the function will return the number deposit amount */
+        return NumberDepositamount;    
+    }
+}
 };
 
-Deposit();
-// if you dont call the function it wont run// 
+const Despositamount = Deposit();
+console.log ( "You have deposited: R " + Despositamount); // This will show the amount that was deposited by the user// 
+// if you dont call the function it wont run // 
 
 
 
